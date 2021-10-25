db6b8b85f0765feed40d72f74dfed04ef0ba57d6
Viktor Bengtsson <vibe16@student.bth.se>
2021-10-25 15:14:55 +0200

    Updated _app.js and sponsorer Routes with framer-motion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import GlobalStyles from '@/styles/GlobalStyles';
 import { appWithTranslation } from 'next-i18next';
+import { AnimateSharedLayout } from 'framer-motion';
 
 const App = ({ Component, pageProps }) => (
-  <>
+  <AnimateSharedLayout>
     <GlobalStyles />
     <Component {...pageProps} />
-  </>
+  </AnimateSharedLayout>
 );
 
 export default appWithTranslation(App);
--- src/pages/sponsorer.js
+++ src/pages/sponsorer.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import tw, { styled } from 'twin.macro';
+import { AnimatePresence, motion } from 'framer-motion';
 import Layout from '@/components/Layout';
 import Section from '@/components/Section';
 import PageTitle from '@/components/PageTitle';
 
-const data = [1, 2, 3, 4, 5, 6];
+const data = [0, 1, 2, 3, 4, 5, 6, 7];
 
 const Card = styled.a(({ selected = false }) => [
   tw`transition duration-300 border h-auto rounded hover:bg-gray-100 cursor-pointer`,
   selected && tw`bg-gray-100`,
 ]);
+const Animate = tw(motion.div)`mt-10 mb-5 shadow-lg rounded border`;
 
 export default function Sponsorer() {
-  const [selected, setSelected] = React.useState(1);
+  const [selected, setSelected] = React.useState(0);
+
+  const Sponsor = ({ sponsor }) => (
+    <AnimatePresence exitBeforeEnter={true}>
+      <Animate
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: 'easeIn', duration: 0.6 }}
+      >
+        <div tw="flex flex-col lg:flex-row lg:divide-x-2">
+          <div tw="w-full lg:w-2/3 p-5 lg:p-10">
+            <h2 tw="text-lg lg:text-xl font-medium pb-4">
+              Företagsnamn {sponsor}
+            </h2>
+            <p tw="pb-4 italic">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <a tw="text-indigo-600 hover:text-indigo-800" href="/">
+              http://sponsorlink.com
+            </a>
+          </div>
+          <div tw="w-full lg:w-1/3 p-5 lg:p-10 order-first lg:order-last">
+            <img src="https://static.wixstatic.com/media/620c21_9e02d1d38f5449b3be2d1661347bdcde~mv2.png/v1/fill/w_440,h_157,al_c,q_85,usm_0.66_1.00_0.01/Black%20logo%20-%20no%20background.webp" />
+          </div>
+        </div>
+      </Animate>
+    </AnimatePresence>
+  );
 
   return (
     <React.Fragment>
@@ -35,26 +67,7 @@ export default function Sponsorer() {
               ))}
             </div>
           </div>
-          <div tw="mt-10 mb-5 shadow-2xl rounded border animate-fadeIn">
-            <div tw="flex flex-col lg:flex-row lg:divide-x-2">
-              <div tw="w-full lg:w-2/3 p-5 lg:p-10">
-                <h2 tw="text-lg lg:text-xl font-medium pb-4">Företagsnamn</h2>
-                <p tw="pb-4 italic">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book.
-                </p>
-                <a tw="text-indigo-600 hover:text-indigo-800" href="/">
-                  http://sponsorlink.com
-                </a>
-              </div>
-              <div tw="w-full lg:w-1/3 p-5 lg:p-10 order-first lg:order-last">
-                <img src="https://static.wixstatic.com/media/620c21_9e02d1d38f5449b3be2d1661347bdcde~mv2.png/v1/fill/w_440,h_157,al_c,q_85,usm_0.66_1.00_0.01/Black%20logo%20-%20no%20background.webp" />
-              </div>
-            </div>
-          </div>
+          <Sponsor sponsor={data[selected]} />
         </Section>
       </Layout>
     </React.Fragment>
